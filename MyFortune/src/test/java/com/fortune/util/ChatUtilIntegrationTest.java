a0270b61bf95f94b9c04bd50d3843e5997b41507
MinjiKim-123 <mmmmin0729@gmail.com>
2025-11-04 12:40:36 +0900

    property 파일 수정 & gemini 통합테스트 오타 수정

--- MyFortune/src/test/java/com/fortune/util/ChatUtilIntegrationTest.java
+++ MyFortune/src/test/java/com/fortune/util/ChatUtilIntegrationTest.java
@@ -19,29 +19,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 class ChatUtilIntegrationTest {
-	
+
 	@Autowired
 	private VertexAiGeminiChatModel geminiChatModel;
-	
+
 	@Autowired
 	private ObjectMapper objectMapper;
-	
+
 	@Test
 	void callGeminiSuccess() throws JsonProcessingException {
 		String userMsg = "오늘의 날짜를 알려주세요";
-		String sysMsg ="""
+		String sysMsg = """
 				아래 json 형식으로 알려줘. 날짜 형식은 yyyyMMdd 패턴에 맞춰서 알려줘"
 				{
 				    "date": "<날짜>"
 				}""";
-		
+
 		ChatResponse chatRes = ChatUtil.call(geminiChatModel, userMsg, sysMsg);
 		String resText = ChatUtil.getResponseText(chatRes);
 		assertNotNull(resText);
-		
+
 		Map<String, String> map = objectMapper.readValue(resText, new TypeReference<Map<String, String>>() {
-		} );
-		assertEquals(LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE), map.get("data"));
+		});
+		assertEquals(LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE), map.get("date"));
 	}
 
 }
